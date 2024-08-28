69ccb3d835aa7a63aefd37fe10ce8f3acd7d1b57
김유정 <aabbc0908@naver.com>
2024-08-28 11:35:17 +0900

    commit

--- CLASS/src/분할정복_0828/퀵정렬01_호어파티션.java
+++ CLASS/src/분할정복_0828/퀵정렬01_호어파티션.java
@@ -14,9 +14,9 @@ public class 퀵정렬01_호어파티션 {
 	
 	static void quickSort(int left, int right) {
 		if(left < right) {
-			int pivot = partition(left, right);
-			quickSort(left, pivot-1);
-			quickSort(pivot+1, right);
+			int pivot = partition(left, right); // 파티션을 하고 다시 새로 기준이 된 피봇을 기준으로
+			quickSort(left, pivot-1); // 왼쪽을 다시 퀵정렬
+			quickSort(pivot+1, right); // 오른쪽을 다시 퀵정렬
 		}
 	}
 	
@@ -32,15 +32,20 @@ public class 퀵정렬01_호어파티션 {
 			// R : pivot 보다 작거나 같은 값을 만날 때까지 이동을 하겠다.
 			while(arr[R] > pivot) R--;
 			
+			// 다 찾았어, 왼쪽 구간에서 pivot보다 큰 값과 오른쪽 구간에서 pivot보다 작은 값을 다 찾았어
+			
+			// 그러면 그 둘의 값을 서로 바꿔
 			if(L < R) {
 				int tmp = arr[L];
 				arr[L] = arr[R];
 				arr[R] = tmp;
 			}
 		}
-		// R의 위치는 사실 피봇이 가야할 위치이다.
-		int tmp = arr[left];
-		arr[left] = arr[R];
+		// L과 R이 역전되면 R을 기준으로 왼쪽은 다 pivot보다 작은 상태, 오른쪽은 다 pivot보다 큰 상태가 된다.
+		
+		// 그러면 이제 R의 위치는 사실 피봇이 가야할 위치이다.
+		int tmp = arr[left]; // pivot을 임시변수에 저장
+		arr[left] = arr[R]; // 오른쪽 구간에서 작았던 값을 저장
 		arr[R] = tmp;
 		
 		return R; // 피봇의 위치

174beca4b3ba04022c4c4b892bbceb6b3f62e1f5
김유정 <aabbc0908@naver.com>
2024-08-28 11:56:04 +0900

    commit

--- CLASS/src/분할정복_0828/퀵정렬02_로무토파티션.java
+++ CLASS/src/분할정복_0828/퀵정렬02_로무토파티션.java
@@ -22,18 +22,23 @@ public class 퀵정렬02_로무토파티션 {
 	
 	// 반환값은 피봇의 위치
 	static int partition(int left, int right) {
-		int pivot = arr[right];
-		int i = left-1;
+		int pivot = arr[right]; // 배열의 맨 마지막 요소가 pivot
+		int i = left-1; // i는 정렬을 해야하는 부분의 왼쪽 끝보다 -1
 		
+		// for문을 돌리다보면 i는 pivot보다 작은 값에 위치하게 되어 있고
+		// j는 계속 한 칸씩 이동한다.
+		// 만약 j가 pivot보다 작으면 둘이 바꿔서 앞쪽에는 작은 값들만 모여있고 뒤에는 큰 값들만 보여있게 된다.
 		for(int j = left; j < right; j++) {
-			if(arr[j] <= pivot) {
-				i++;
-				int tmp = arr[i];
+			if(arr[j] <= pivot) { // j가 pivot 이하이면 
+				i++; // i를 1 증가하고 
+				int tmp = arr[i]; // i와 j의 값을 바꿈
 				arr[i] = arr[j];
 				arr[j] = tmp;
 			}
 		}
 		
+		// 정렬이 다 끝나고 나면
+		// 작은 값들이 모여있는 i까지의 구간에서 i+1에 피벗을 넣어준다.
 		int tmp = arr[i+1];
 		arr[i+1] = arr[right];
 		arr[right] = tmp;

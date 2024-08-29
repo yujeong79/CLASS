8105aa5f42013f2f15d20737488f8f5fc222ce97
김유정 <aabbc0908@naver.com>
2024-08-29 10:13:05 +0900

    commit

--- CLASS/src/백트래킹_0829/순열01_반복문.java
+++ CLASS/src/백트래킹_0829/순열01_반복문.java
@@ -8,6 +8,7 @@ public class 순열01_반복문 {
 		nums = new int[] {1, 2, 3};
 		N = nums.length;
 		
+		// 이렇게 하면 요소가 많아질 수록 하기가 어려워진다.
 		for(int i = 0; i < N; i++) {
 			for(int j = 0; j < N; j++) {
 				if(i != j) // 요소의 중복을 제거하기 위함

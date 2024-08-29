9c6f5b9a0ebd382e3a459a2172b27c8be73ada35
김유정 <aabbc0908@naver.com>
2024-08-29 10:12:43 +0900

    commit

--- CLASS/src/백트래킹_0829/순열01_반복문.java
+++ CLASS/src/백트래킹_0829/순열01_반복문.java
@@ -8,11 +8,12 @@ public class 순열01_반복문 {
 		nums = new int[] {1, 2, 3};
 		N = nums.length;
 		
-		// 중복 순열
 		for(int i = 0; i < N; i++) {
 			for(int j = 0; j < N; j++) {
+				if(i != j) // 요소의 중복을 제거하기 위함
 				for(int k = 0; k < N; k++) {
-					System.out.println(nums[i] + ", " + nums[j] + ", " + nums[k]);
+					if(i != k && j != k)
+						System.out.println(nums[i] + ", " + nums[j] + ", " + nums[k]);
 				}
 			}
 		}

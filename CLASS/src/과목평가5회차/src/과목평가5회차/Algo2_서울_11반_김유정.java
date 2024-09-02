46a703cc1c96f5befc96ce64a76bd69d50841b3b
yujeong79 <77478845+yujeong79@users.noreply.github.com>
2024-09-02 12:13:07 +0900

    Add files via upload

---
+++ CLASS/src/과목평가5회차/src/과목평가5회차/Algo2_서울_11반_김유정.java
@@ -0,0 +1,71 @@
+package 과목평가5회차;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Algo2_서울_11반_김유정 {
+	private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); // 입력을 받기 위한 BufferedReader
+	private static StringBuilder sb = new StringBuilder(); // 각 테스트케이스의 출력을 한 번에 모아서 출력하기 위한 StringBuilder
+	
+	private static int N; // 마라톤에 출전한 N명의 선수
+	private static int M; // 선물을 받는 M명의 선수
+	private static int K; // M명의 등번호의 합
+	
+	private static int[] nums; // N명의 선수의 등번호
+	private static int[] result; // N명의 선수 중 M명의 선수의 조합을 임시로 저장하기 위한 배열
+	private static boolean[] isSelected; // 각각의 선수가 조합에 들어갔는지 확인하기 위한 배열
+	 
+	private static int answer; // 등번호의 합이 K가 되는 M명의 선수 조합의 경우의 수를 저장하기 위한 변수
+	
+	public static void main(String[] args) throws IOException {
+		int T = Integer.parseInt(br.readLine()); // 전체 테스트케이스의 수
+		int testCase = 0; // 각 테스트케이스
+		while(++testCase <= T) { // 각 테스트케이스가 전체 테스트케이스 수만큼 진행됐으면 종료
+			StringTokenizer st = new StringTokenizer(br.readLine(), " "); // N, M, K를 한 번에 받기 위한 StringTokenizer, 공백을 기준으로 나눔
+			N = Integer.parseInt(st.nextToken()); // 마라톤에 출전한 N명의 선수
+			M = Integer.parseInt(st.nextToken()); // 선물을 받는 M명의 선수
+			K = Integer.parseInt(st.nextToken()); // M명의 등번호의 합
+			
+			nums = new int[N]; // 마라톤의 출전한 선수들의 등번호를 저장하기 위한 배열의 크기를 N으로 할당
+			st = new StringTokenizer(br.readLine(), " "); // 선수들의 등번호를 한 번에 받기 위한 StringTokenizer
+			for(int i = 0; i < N; i++) { // 선수들의 수 만큼 순회를 돌며
+				nums[i] = Integer.parseInt(st.nextToken()); // 배열에 선수들의 등번호를 저장
+			} 
+			
+			answer = 0; // 0으로 초기화
+			isSelected = new boolean[N]; // 선수가 조합에 들어갔는지 확인하기 위한 배열도 N 크기로 초기화
+			result = new int[M]; // 조합을 임시로 저장하기 위한 배열도 조합의 크기인 M으로 초기화
+			perm(0); // 순열
+			
+			answer = answer == 0 ? -1 : answer; // 등번호의 합 K에 해당하는 어떠한 경우의 수도 없다면 answer에 -1 저장
+			sb.append("#" + testCase + " " + answer + "\n"); // 각 테스트케이스의 출력값 저장
+		} // end of testCase
+		System.out.println(sb); // 모든 테스트케이스의 출력값 출력
+	} // end of main
+
+	/**
+	 * N명의 선수 중 M명의 선수 조합 뽑기
+	 * 같은 선수로 구성된 조합이어도 순위에 따라 다른 경우의 수로 판단되기 때문에 순열로 동작
+	 * @param cnt : 뽑힌 선수의 수
+	 */
+	private static void perm(int cnt) { 
+		if(cnt == M) { // M명의 선수들이 뽑혔으면
+			int sum = 0; // 뽑힌 M명의 선수들의 등번호의 합을 저장하기 위한 변수
+			for(int n : result) sum += n; // M명의 선수들의 등번호를 sum에 누적
+			if(sum == K) answer++; // sum이 K와 같으면 answer를 1 증가
+			return; // 종료
+		}
+		
+		for(int i = 0; i < N; i++) { // N명의 선수들을 순회하며
+			if(!isSelected[i]) { // i번째 선수가 선택되지 않았으면
+				isSelected[i] = true; // 선택하고
+				result[cnt] = nums[i]; // M명의 선수들 조합에 넣어주기
+				perm(cnt+1); // 다음 선수 뽑기
+				isSelected[i] = false; // 다시 다른 조합을 뽑기 위해서 해당 선수를 다시 false로 바꿔주기
+			}
+		}
+	}
+} // end of class
+
---
+++ CLASS/src/과목평가5회차/src/과목평가5회차/Algo3_서울_11반_김유정.java
@@ -0,0 +1,81 @@
+package 과목평가5회차;
+
+/**
+ * 1) 병합 정렬의 pseudocode 또는 java 코드를 작성하시오.
+ */
+
+import java.util.Arrays;
+
+public class Algo3_서울_11반_김유정 {
+	private static int[] nums = {6, 8, 1, 10, 3, 5, 7, 4, 9, 2}; // 정렬하고자 하는 배열
+	private static int[] temp = new int[nums.length]; // 병합 정렬을 위한 임시 배열
+
+	public static void main(String[] args) {
+		mergeSort(0, nums.length-1); // 병합 정렬 메소드 호출
+		
+		System.out.println(Arrays.toString(nums)); // 오름차순으로 정렬된 배열 호출
+	}
+
+	/**
+	 * 병합 정렬 메소드
+	 * @param left : 정렬하고자 하는 구간의 가장 왼쪽 인덱스
+	 * @param right : 정렬하고자 하는 구간의 가장 오른쪽 인덱스
+	 */
+	private static void mergeSort(int left, int right) { 
+		if(left >= right) return; // left >= right가 되면 더 이상 정렬할 수가 없다는 뜻이므로 return 
+		
+		int mid = (left+right)/2; // 정렬하고자 하는 구간의 가운데 인덱스를 저장
+		mergeSort(left, mid); // 가운데 인덱스를 기준으로 왼쪽 구간을 병합 정렬
+		mergeSort(mid+1, right); // 가운데 인덱스를 기준으로 오른쪽 구간을 병합 정렬
+		merge(left, mid, right); // mergeSort로 정렬된 왼쪽 구간과 오른쪽 구간을 병합
+	}
+
+	/**
+	 * 정렬된 왼쪽 구간과 오른쪽 구간을 병합
+	 * @param left : 합치고자 하는 구간의 가장 왼쪽 인덱스
+	 * @param mid : 합치고자 하는 구간의 가운데 인덱스
+	 * @param right : 합치고자 하는 구간의 가장 오른쪽 인덱스
+	 */
+	private static void merge(int left, int mid, int right) {
+		int L = left; // 왼쪽 구간의 가장 왼쪽 인덱스
+		int R = mid+1; // 오른쪽 구간의 가장 왼쪽 인덱스
+		
+		int idx = left; // 병합을 위한 임시 배열 temp의 인덱스, 초기에는 정렬하고자 하는 구간의 맨 왼쪽 인덱스로 초기화
+		
+		while(L <= mid && R <= right) { // 왼쪽 구간과 오른쪽 구간 모두 아직 다 정렬이 안되었으면
+			if(nums[L] <= nums[R]) temp[idx++] = nums[L++]; // 왼쪽 구간의 수가 오른쪽 구간의 수보다 작으면 임시배열에 왼쪽 구간의 수를 넣고 idx와 L을 한 칸 이동
+			else temp[idx++] = nums[R++]; // 오른쪽 구간의 수가 더 작으면 임시배열에 오른쪽 구간의 수를 넣고 idx와 R을 한 칸 이동
+		}
+		
+		// 왼쪽 구간이나 오른쪽 구간 둘 중 한 구간의 정렬이 다 완료 되었으면
+		// 나머지 구간의 원소를 다 털기
+		
+		if(L <= mid) { // 아직 왼쪽이 남았으면
+			for(int i = L; i <= mid; i++) { // 남은 왼쪽 구간의 수를 다 순회하며
+				temp[idx++] = nums[i]; // temp에 넣기
+			}
+		} else { // 아직 오른쪽이 남았으면
+			for(int i = R; i <= right; i++) { // 남은 오른쪽 구간의 수를 다 순회하며
+				temp[idx++] = nums[i]; // temp에 넣기
+			}
+		}
+		
+		for(int i = left; i <= right; i++) { // 정렬된 수를 임시로 저장한 완성된 temp를
+			nums[i] = temp[i]; // 기존의 nums 배열에 옮기기
+		}
+	}
+}
+
+/**
+ * 2) 병합 정렬의 특징을 아는 대로 서술하시오. (시간 복잡도, 공간 복잡도, 다른 정렬과의 비교, 구현상에 있어서의 특징 등)
+ * 	병합 정렬의 시간 복잡도는 O(NlogN)이다.
+ * 
+ * 	병합 정렬은 정렬 시에 정렬하고자 하는 구간의 정렬된 원소를 임시로 저장할 임시 배열이 필요하다. 
+ * 
+ * 	다른 정렬과 비교 했을 때 병합 정렬은 평균 O(NlogN)이지만 최악에는 O(N^2)의 시간 복잡도를 보여주는 퀵 정렬과 달리 
+ * 	최악의 상황에서도 시간 복잡도가 O(NlogN)이라는 점에서 최악의 상황에서도 빠르게 정렬할 수 있다는 장점이 있다.
+ * 
+ *  병합 정렬은 정렬하고자 하는 배열을 가운데를 기준으로 왼쪽 구간과 오른쪽 구간으로 나눌 수 없을 때까지 나눈 뒤 각 구간을 정렬하고,
+ *  정렬된 왼쪽 구간과 오른쪽 구간을 다시 정렬하면서 합치는 방식으로 동작한다. 
+ */
+

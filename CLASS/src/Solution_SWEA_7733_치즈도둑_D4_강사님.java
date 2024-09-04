9424857e2a23c52afa1881b164a72b2be9dbeffa
김유정 <aabbc0908@naver.com>
2024-09-04 13:50:14 +0900

    commit

---
+++ CLASS/src/Solution_SWEA_7733_치즈도둑_D4_강사님.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+/**
+ * 시뮬레이션, BFS, DFS
+ */
+
+public class Solution_SWEA_7733_치즈도둑_D4_강사님 {
+	private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	private static StringBuilder sb = new StringBuilder();
+	
+	public static void main(String[] args) throws IOException {
+		int T = Integer.parseInt(br.readLine());
+		int testCase = 0;
+		while(++testCase <= T) {
+			
+		} // end of testCase
+	} // end of main
+} // end of class

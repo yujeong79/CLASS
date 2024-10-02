77dda3bdaf7ed8c6b415b430e6e1afee4b81b5ee
김유정 <aabbc0908@naver.com>
2024-10-02 15:42:18 +0900

    commit

--- CLASS/src/월말평가3회차/Test1_서울_11_김유정.java
+++ CLASS/src/월말평가3회차/Test1_서울_11_김유정.java
@@ -4,7 +4,29 @@ import java.io.*;
 import java.util.*;
 
 public class Test1_서울_11_김유정 {
-	public static void main(String[] args) {
-		
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringBuilder sb = new StringBuilder();
+	
+	static final int[] stairs = {1, 3, 4};
+	static int H;
+	
+	public static void main(String[] args) throws IOException {
+		int T = Integer.parseInt(br.readLine());
+		int testCase = 0;
+		while(++testCase <= T) {
+			H = Integer.parseInt(br.readLine());
+			
+			int[] dp = new int[H+1];
+			dp[0] = 1;
+			
+			for(int s : stairs) {
+				for(int i = s; i <= H; i++) {
+					dp[i] += dp[i-s];
+				}
+			}
+			
+			sb.append("#" + testCase + " " + dp[H] + "\n");
+		} // end of testCase
+		System.out.println(sb);
 	} // end of main
 } // end of class
